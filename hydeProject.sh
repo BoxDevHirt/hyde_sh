73b8949639c49f4cc2124e1bcc575578709199a2
Vitor Hirt <vitordev.webdeveloper@outlook.com>
2025-07-26 15:05:00 -0300

    Hyde install

---
+++ hydeProject.sh
@@ -0,0 +1,9 @@
+
+# git && base-devel
+sudo pacman -S --needed git base-devel
+# clone repositorie
+git clone --depth 1 https://github.com/HyDE-Project/HyDE ~/HyDE
+# folder project
+cd ~/HyDE/Scripts
+# install hyde
+./install.sh
